363b47e7b081d7c4e05841c64225666cb47445ff
Damx9007 <damx9007@gmail.com>
2022-07-25 11:14:58 +0200

    Event Listeners adding

---
+++ ui/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    function display(bool) {
+        if (bool) {
+            $('#container').show();
+        } else {
+            $('#container').hide();
+        }
+    }
+
+
+    // ESEMÉNY FIGYELÉS 
+    window.addEventListener('message', function(e){
+        let data = e.data;
+
+        if (data.type === "ui") {
+            if (data.status == true) {
+                display(true)
+            } else {
+                display(false)
+            }
+        }
+    })
+
+    document.onkeyup = (data) => {
+        if (data.key == "Escape") {
+            $.post('http://damx_pedset/exit', JSON.stringify({}));
+            return
+        }
+    }
+
+
+
+
+
+
+})
+
+
+
+
+
